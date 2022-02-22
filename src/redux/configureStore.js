3b19ca0ddbb3537448af4250d5fb1165889169aa
Billions Joel <joelatugonza@gmail.com>
2022-02-22 18:29:30 +0300

    Import books reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'react-redux';
 import logger from 'redux-logger';
+import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
