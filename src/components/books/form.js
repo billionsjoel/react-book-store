b3c7fc2ca06d2cc064d7d49e1436f2ec8ccbdd35
Billions Joel <joelatugonza@gmail.com>
2022-02-22 16:30:02 +0300

    Add style for form

--- src/components/books/form.js
+++ src/components/books/form.js
@@ -1,3 +1,4 @@
+import './css/form.scss';
 import React from 'react';
 
 function form() {
@@ -5,21 +6,21 @@ function form() {
     <div className="form-container">
       <h3 className="form-title">ADD NEW BOOK</h3>
       <div className="form">
-        <div className="form-check">
-          <input
-            type="text"
-            className="form-check-input"
-            name=""
-            id=""
-            placeholder="Book title"
-          />
-          <select name="categories" id="category-id">
-            <option value="Action">Action</option>
-            <option value="Fiction">Fiction</option>
-            <option value="Economy">Economy</option>
-          </select>
-          <button type="button" value="" className="btn">ADD BOOK</button>
-        </div>
+        <input
+          type="text"
+          className="input-title"
+          name=""
+          id=""
+          placeholder="Book title"
+        />
+        <select name="categories" id="category-id" className="input-categories">
+          <option value="Action">Action</option>
+          <option value="Fiction">Fiction</option>
+          <option value="Economy">Economy</option>
+        </select>
+        <button type="button" value="" className="btn-submit">
+          ADD BOOK
+        </button>
       </div>
     </div>
   );
