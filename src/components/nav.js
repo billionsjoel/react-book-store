4b7e56a38898879c6fb5e9ac2689383f40cc9b54
Billions Joel <joelatugonza@gmail.com>
2022-02-22 11:38:37 +0300

    Fix component issues

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function nav() {
-  return (
-    <div>nav</div>
-  )
+  return <div>nav</div>;
 }
 
-export default nav
+export default nav;
---
+++ src/components/panel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function panel() {
+  return <div>panel</div>;
+}
+
+export default panel;
