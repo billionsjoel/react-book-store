96aab0a6576ef88c440422dcf08b310d5bc4dc90
Billions Joel <joelatugonza@gmail.com>
2022-02-23 13:44:20 +0300

    Import useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/books/nav';
 import Category from './components/categories/category';
